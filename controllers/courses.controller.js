596e7420497e3987efc91918949a440cf5ff7480
rakibul58 <rhrahi14@gmail.com>
2023-10-20 11:42:00 +0600

    get course by id

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -3,8 +3,14 @@ const client = require("../utils/dbConnect");
 const courseCollection = client.db('experiment-labs').collection('courses');
 
 module.exports.getAllCourses = async (req, res, next) => {
-
     const courses = await courseCollection.find({}).toArray();
     res.send(courses);
-
 };
+
+
+module.exports.getACourseById = async (req, res, next) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const course = await courseCollection.findOne(filter);
+    res.send(course);
+}
