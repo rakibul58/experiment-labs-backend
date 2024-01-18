043db71f0056960ac7f2034ebd474ab64fc6c16c
Shajibul Alam Shihab <shihab77023@gmail.com>
2024-01-18 18:16:09 +0600

    update

--- routes/v1/users.route.js
+++ routes/v1/users.route.js
@@ -4,22 +4,20 @@ const userControllers = require("../../controllers/users.controller");
 const router = express.Router();
 
 router
-    .route("/")
-    .get(userControllers.getAnUserByEmail)
-    .post(userControllers.saveAUser);
+  .route("/")
+  .get(userControllers.getAnUserByEmail)
+  .post(userControllers.saveAUser);
 
 router
-    .route("/mentors/organizationId/:organizationId")
-    .get(userControllers.getAllMentors);
+  .route("/mentors/organizationId/:organizationId")
+  .get(userControllers.getAllMentors);
 
+router.route("/unpaidUsers/checkout").post(userControllers.checkoutPayment);
 
-router
-    .route("/unpaidUsers/checkout")
-    .post(userControllers.checkoutPayment);
+router.route("/unpaidUsers/verifyPayment").post(userControllers.verifyPayment);
 
-router
-    .route("/unpaidUsers/verifyPayment")
-    .post(userControllers.verifyPayment);
+router.route("/addStudent").post(userControllers.addStudent);
 
+router.route("/addBulkStudent").post(userControllers.addBulkStudent);
 
 module.exports = router;
--- utils/generatePassword.js
+++ utils/generatePassword.js
@@ -1,11 +1,11 @@
 const generateCustomPassword = (student) => {
-    const { firstName, phone } = student;
-    const formattedFirstName = firstName.replace(/\s/g, '');
-    const phoneString = phone + "";
-    const lastFourDigits = phoneString.slice(-4);
-    const password = `${formattedFirstName}_${lastFourDigits}`;
-    return password;
+  const { name, phone } = student;
+  const formattedName = name.replace(/\s/g, "");
+  const phoneString = phone + "";
+  const lastFourDigits = phoneString.slice(-4);
+  const password = `${formattedName}_${lastFourDigits}`;
+  return password;
 };
 module.exports = {
-    generateCustomPassword,
+  generateCustomPassword,
 };
