a5133d05c1b72502261ab8316d2bfb6a545ae774
Shajibul Alam Shihab <shihab77023@gmail.com>
2024-02-05 17:42:31 +0600

    update

--- controllers/stats.controller.js
+++ controllers/stats.controller.js
@@ -1,9 +1,10 @@
 const { ObjectId } = require("mongodb");
 const client = require("../utils/dbConnect");
 const userCollection = client.db("experiment-labs").collection("users");
-const assignmentSubmitCollection = client.db('experiment-labs').collection('assignments-submit');
-const eventCollection = client.db('experiment-labs').collection('events');
-
+const assignmentSubmitCollection = client
+  .db("experiment-labs")
+  .collection("assignments-submit");
+const eventCollection = client.db("experiment-labs").collection("events");
 
 /*   module.exports.getUsersByOrganizationId = async (req, res) => {
     try {
@@ -21,46 +22,70 @@ const eventCollection = client.db('experiment-labs').collection('events');
     }
   }; */
 
-
 module.exports.getUsersByOrganizationId = async (req, res, next) => {
-    try {
-        const organizationId = req.params.organizationId;
+  try {
+    const organizationId = req.params.organizationId;
 
-        // Query for users with the role "user"
-        const userQuery = { organizationId: organizationId, role: "user" };
-        const totalStudent = await userCollection.countDocuments(userQuery);
+    // Query for users with the role "user"
+    const userQuery = { organizationId: organizationId, role: "user" };
+    const totalStudent = await userCollection.countDocuments(userQuery);
 
-        // Query for users with non-empty "courses" array
-        const courseQuery = { organizationId: organizationId, role: "user", courses: { $exists: true, $ne: [] } };
-       
-        const enrollStudents = await userCollection.countDocuments(courseQuery);
+    // Query for users with non-empty "courses" array
+    const courseQuery = {
+      organizationId: organizationId,
+      role: "user",
+      courses: { $exists: true, $ne: [] },
+    };
 
-        // Query for students with unevaluated result array
-        const resultQuery = { "submitter.organizationId": organizationId, "submitter.role": "user", "submitter.result" : { $exists: false } };
-     
-        const unevaluatedStudents = await assignmentSubmitCollection.countDocuments(resultQuery);
+    const enrollStudents = await userCollection.countDocuments(courseQuery);
+    const enrollStudentsData = await userCollection.find(courseQuery).toArray();
+    // console.log(enrollStudentsData);
 
-        // Query for total meeting
-         // Get the start of the current day
-         const currentDate = new Date();
-         const formattedCurrentDate = currentDate.toISOString().slice(0, 10);
-        
-        
-        
-         // Query for total meetings within today
-         const meetingQuery = {
-             "organization.organizationId": organizationId,
-             
-             start: { $regex: `^${formattedCurrentDate}` },
-         }
-     
-    
-        const totalMeeting = await eventCollection.countDocuments(meetingQuery);
+    const totalPaidAmount = enrollStudentsData.reduce((total, student) => {
+      if (student.courses && Array.isArray(student.courses)) {
+        student.courses.forEach((course) => {
+          if (course.paidAmount) {
+            total += course.paidAmount;
+          }
+        });
+      }
+      return total;
+    }, 0);
 
-        res.send({ totalStudent, enrollStudents, unevaluatedStudents,totalMeeting });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
-    }
+    // Query for students with unevaluated result array
+    const resultQuery = {
+      "submitter.organizationId": organizationId,
+      "submitter.role": "user",
+      "submitter.result": { $exists: false },
+    };
+
+    const unevaluatedStudents = await assignmentSubmitCollection.countDocuments(
+      resultQuery
+    );
+
+    // Query for total meeting
+    // Get the start of the current day
+    const currentDate = new Date();
+    const formattedCurrentDate = currentDate.toISOString().slice(0, 10);
+
+    // Query for total meetings within today
+    const meetingQuery = {
+      "organization.organizationId": organizationId,
+
+      start: { $regex: `^${formattedCurrentDate}` },
+    };
+
+    const totalMeeting = await eventCollection.countDocuments(meetingQuery);
+
+    res.send({
+      totalStudent,
+      enrollStudents,
+      unevaluatedStudents,
+      totalMeeting,
+      totalPaidAmount,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 };
-
