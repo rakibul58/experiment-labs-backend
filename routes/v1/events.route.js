9418fc5ad770954e40fd2d4a9eb9ab332bea791c
Shajibul Alam Shihab <shihab77023@gmail.com>
2024-02-09 14:39:27 +0600

    update

--- routes/v1/events.route.js
+++ routes/v1/events.route.js
@@ -4,14 +4,12 @@ const eventControllers = require("../../controllers/events.controller");
 const router = express.Router();
 
 router
-    .route("/")
-    .get(eventControllers.getAllEvent)
-    .post(eventControllers.addAnEvent);
+  .route("/")
+  .get(eventControllers.getAllEvent)
+  .post(eventControllers.addAnEvent);
 
+router.route("/:id").get(eventControllers.getAnEvent);
 
-router
-    .route("/:id")
-    .get(eventControllers.getAnEvent);
-
+router.route("/email/:email").get(eventControllers.getEventsByEmail);
 
 module.exports = router;
