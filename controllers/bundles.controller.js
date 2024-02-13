f06114dfad956847cd4c41381210244e15ec42e1
Shajibul Alam Shihab <shihab77023@gmail.com>
2024-02-13 17:47:03 +0600

    update

---
+++ controllers/bundles.controller.js
@@ -0,0 +1,28 @@
+const { ObjectId } = require("mongodb");
+const client = require("../utils/dbConnect");
+const bundleCollection = client.db("experiment-labs").collection("bundles");
+
+module.exports.addABundle = async (req, res, next) => {
+  const bundle = req.body;
+  const result = await bundleCollection.insertOne(bundle);
+  res.send(result);
+};
+
+module.exports.getABundle = async (req, res, next) => {
+  const id = req.params.bundleId;
+  const filter = { _id: new ObjectId(id) };
+  const result = await bundleCollection.findOne(filter);
+  res.send(result);
+};
+
+module.exports.getBundlesByOrganizationId = async (req, res, next) => {
+  try {
+    const id = req.params.organizationId;
+    const filter = { organizationId: id };
+    const result = await bundleCollection.find(filter).toArray();
+    res.status(200).json(result);
+  } catch (error) {
+    console.error("Error fetching user notifications:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
