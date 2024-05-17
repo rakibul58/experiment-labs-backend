f45d95ca6a7addecc73ab819f96919d1aacccf1d
Shajibul Alam Shihab <shihab77023@gmail.com>
2024-05-17 10:50:21 +0600

    update

--- routes/v1/quizzes.route.js
+++ routes/v1/quizzes.route.js
@@ -3,15 +3,15 @@ const quizControllers = require("../../controllers/quizzes.controller");
 
 const router = express.Router();
 
-router
-    .route("/:id")
-    .post(quizControllers.addAQuestionToQuiz);
-
+router.route("/:id").post(quizControllers.addAQuestionToQuiz);
 
 router
-    .route("/:id/questionId/:questionId")
-    .get(quizControllers.getAQuestionByQuizIdAndQuestionId)
-    .put(quizControllers.updateAQuestionFromQuiz);
+  .route("/:id/questionId/:questionId")
+  .get(quizControllers.getAQuestionByQuizIdAndQuestionId)
+  .put(quizControllers.updateAQuestionFromQuiz);
 
+router
+  .route("/questionId/:questionId")
+  .put(quizControllers.updateQuizQuestions);
 
 module.exports = router;
